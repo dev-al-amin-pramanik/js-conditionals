7962646eb5fa066c9216214f4b560babc9cedae0
Al Amin <alamin@Als-MacBook-Air.local>
2025-02-11 20:45:45 +0600

    added if condition

--- if.js
+++ if.js
@@ -1,3 +1,42 @@
 /**
  * If you are pay the bill then i will come
+ * 
+ * -------------------------------------
+ * if biriyani price less than 300 taka then i will buy
+ * 
+ * 
+ * 
+ * -------------STRUCTURE-----------------
+ * if (condition){
+ * }
+ * 
+ * 
 */
+
+if (3 < 10) {
+    console.log('i have a smaller value');
+}
+
+var biriyaniPrice = 500;
+
+if (biriyaniPrice < 300) {
+    console.log('i will buy one');
+}
+
+/**
+ * if (condition) {
+ *    execute code if condition above is true
+ * }
+ * else{
+ * execute code if the condition is false
+ * }
+ * */ 
+
+var weight = 10;
+
+if (weight < 20) {
+    console.log(' I will carry it by myself')
+}
+else {
+    console.log('i will rent a rickshaw')
+}
---
+++ multiple.js
@@ -0,0 +1,32 @@
+const salary = 35000;
+const isBCS = true;
+const height = 61;
+const hasCar = false;
+
+// if (salary > 20000 && height >66) {
+//     console.log('Su------patro');
+// }
+// else {
+//     console.log('onno patro khuji');
+// }
+
+// if (salary > 25000 || height > 72) {
+//     console.log('eso baba kobul');
+// }
+// else {
+//     console.log('vag baba mokbul');
+// }
+
+// more and more CSSCondition
+// if (salary > 25000 || height > 72 || isBCS == true) {
+//     console.log('eso baba');
+// }
+// else { 
+//     console.log('vag tui mokbul');
+// }
+
+
+// -----------------------COMPLEX CONDITION--------------------------
+if ((salary > 25000 && hasCar == true) || isBCS == true) {
+    console.log('tomar 14 gusti raji');
+}
